da4a4f09abd1a8bfe1e302a9c2dd48fae68b995c
hrothwell <hrothwell@css.edu>
2022-09-27 19:14:18 -0700

    spacing fix?

--- src/main/kotlin/com/hrothwell/domain/mal/ListStatus.kt
+++ src/main/kotlin/com/hrothwell/domain/mal/ListStatus.kt
@@ -1,8 +1,6 @@
 package com.hrothwell.domain.mal
 
-enum class ListStatus(
-	val malValue: String
-) {
+enum class ListStatus(val malValue: String) {
 	COMPLETED("completed"),
 	PLAN_TO_WATCH("plan_to_watch"),
 	WATCHING("watching"),
